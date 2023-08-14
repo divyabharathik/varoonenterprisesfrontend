29285ef78270fd3116069cc05a2c2c055b59e3f3
divyabharathik <divya.bhaarathy@gmail.com>
2023-08-14 22:12:01 +0530

    changes

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -24,7 +24,7 @@ export class ProductService {
       scientificName: 'Vitis vinifera',
       color: '#B6B8B5B0',
       imageLink: "../../assets/images/grapes.jpg",
-      description: "Delight in the lusciousness of our premium grapes. Cultivated in vineyards, our grapes are nurtured without synthetic pesticides or chemicals, resulting in juicy and flavorful fruits. Experience the natural sweetness and crisp texture of our grapes, a healthy and refreshing treat straight from the vine.",
+      description: "Delight in the lusciousness of our premium grapes. Cultivated in vineyards, our grapes are nurtured without harmful chemicals, resulting in juicy and flavorful fruits. Experience the natural sweetness and crisp texture of our grapes, a healthy and refreshing treat straight from the vine.",
       category: 'fruits'
     },
     {
@@ -33,7 +33,7 @@ export class ProductService {
       scientificName: 'Musa acuminata',
       color: '#F4D15BB0',
       imageLink: "../../assets/images/banana.jpg",
-      description: "Experience the perfection of bananas with our premium selection.Grown in plantations, our bananas are free from synthetic pesticides and chemicals, offering a natural and wholesome taste.Enjoy the creamy texture and sweet flavor of our bananas, a nutritious choice for your well- being.",
+      description: "Experience the perfection of bananas with our premium selection.Grown in plantations, our bananas are free from harmful chemicals, offering a natural and wholesome taste.Enjoy the creamy texture and sweet flavor of our bananas, a nutritious choice for your well- being.",
       category: 'fruits'
     },
     {
@@ -42,7 +42,7 @@ export class ProductService {
       scientificName: 'Citrus sinensis',
       color: '#FE8D00B0',
       imageLink: "../../assets/images/orange.jpg",
-      description: "Indulge in the tangy sweetness of our premium oranges. Grown in orchards, our oranges are free from synthetic pesticides and genetically modified organisms. Bursting with juicy flavor and packed with vitamin C, our oranges are a refreshing and healthy choice for citrus lovers.",
+      description: "Indulge in the tangy sweetness of our premium oranges. Grown in orchards, our oranges are free from harmful genetically modified organisms. Bursting with juicy flavor and packed with vitamin C, our oranges are a refreshing and healthy choice for citrus lovers.",
       category: 'fruits'
     },
     {
@@ -51,7 +51,7 @@ export class ProductService {
       scientificName: 'Malus pumila',
       color: '#E85E4BB0',
       imageLink: "../../assets/images/apple.jpg",
-      description: "Experience the crisp and natural goodness of our premium apples. Grown in orchards with utmost care, our apples are free from synthetic pesticides and GMOs. Savor the juicy and flavorful bite of our apples, a nutritious and delightful choice for a healthy lifestyle.",
+      description: "Experience the crisp and natural goodness of our premium apples. Grown in orchards with utmost care, our apples are free from harmful GMOs. Savor the juicy and flavorful bite of our apples, a nutritious and delightful choice for a healthy lifestyle.",
       category: 'fruits'
     },
     {
@@ -60,7 +60,7 @@ export class ProductService {
       scientificName: 'Citrullus lanatus',
       color: '#86974DB0',
       imageLink: "../../assets/images/watermelon.jpg",
-      description: "Quench your thirst with the refreshing sweetness of our premium watermelons. Grown in fields, our watermelons are free from synthetic pesticides, ensuring pure and juicy flavor. Experience the burst of hydrating delight and natural goodness with our watermelons, a summer favorite for all.",
+      description: "Quench your thirst with the refreshing sweetness of our premium watermelons. Grown in fields, our watermelons are free from harmful chemicals, ensuring pure and juicy flavor. Experience the burst of hydrating delight and natural goodness with our watermelons, a summer favorite for all.",
       category: 'fruits'
     },
     {
@@ -69,7 +69,7 @@ export class ProductService {
       scientificName: 'Carica papaya',
       color: '#EB8F52B0',
       imageLink: "../../assets/images/papaya.jpg",
-      description: "Experience the tropical delight of our premium papayas. Grown in farms, our papayas are naturally ripened and free from synthetic pesticides. Enjoy the luscious and juicy texture, along with the rich flavor and numerous health benefits of our papayas.",
+      description: "Experience the tropical delight of our premium papayas. Grown in farms, our papayas are naturally ripened and free from harmful chemicals. Enjoy the luscious and juicy texture, along with the rich flavor and numerous health benefits of our papayas.",
       category: 'fruits'
     },
     {
@@ -78,7 +78,7 @@ export class ProductService {
       scientificName: 'Ananas comosus',
       color: '#FFD54EB0',
       imageLink: "../../assets/images/pineapple.jpg",
-      description: "Savor the tropical sweetness of our premium pineapples. Grown in plantations, our pineapples are free from synthetic pesticides, ensuring a pure and juicy taste. Experience the tangy and refreshing flavor, along with the numerous health benefits of our pineapples, straight from the tropical paradise.",
+      description: "Savor the tropical sweetness of our premium pineapples. Grown in plantations, our pineapples are free from harmful chemicals, ensuring a pure and juicy taste. Experience the tangy and refreshing flavor, along with the numerous health benefits of our pineapples, straight from the tropical paradise.",
       category: 'fruits'
     },
     {
@@ -87,7 +87,7 @@ export class ProductService {
       scientificName: 'Punica granatum',
       color: '#EF6959B0',
       imageLink: "../../assets/images/pomegranate.jpg",
-      description: "Indulge in the vibrant and antioxidant-rich flavor of our premium pomegranates. Grown in orchards, our pomegranates are nurtured without synthetic pesticides, delivering a naturally sweet and juicy taste. Experience the delightful burst of refreshing goodness and health benefits with our pomegranates.",
+      description: "Indulge in the vibrant and antioxidant-rich flavor of our premium pomegranates. Grown in orchards, our pomegranates are nurtured without harmful chemicals, delivering a naturally sweet and juicy taste. Experience the delightful burst of refreshing goodness and health benefits with our pomegranates.",
       category: 'fruits'
     },
     {
@@ -96,7 +96,7 @@ export class ProductService {
       scientificName: 'Allium cepa',
       color: '#FFD2DBB0',
       imageLink: "../../assets/images/onion.jpg",
-      description: "Discover the exceptional quality of our premium onions. Grown in fields, our onions are cultivated without synthetic pesticides, resulting in flavorful and nutritious bulbs. Experience the crisp texture and aromatic flavor of our onions, a versatile ingredient that adds depth to a variety of culinary creations.",
+      description: "Discover the exceptional quality of our premium onions. Grown in fields, our onions are cultivated without harmful chemicals, resulting in flavorful and nutritious bulbs. Experience the crisp texture and aromatic flavor of our onions, a versatile ingredient that adds depth to a variety of culinary creations.",
       category: "vegetables"
     },
     {
@@ -105,7 +105,7 @@ export class ProductService {
       scientificName: 'Allium ascalonicum',
       color: '#F2ABCBB0',
       imageLink: "../../assets/images/shallots.jpg",
-      description: "Experience the gourmet excellence of our premium shallots. Cultivated in fields, our shallots are renowned for their rich flavor and delicate aroma. Grown without synthetic pesticides, our shallots offer a unique taste and culinary versatility. Elevate your dishes with the exceptional quality of our shallots.",
+      description: "Experience the gourmet excellence of our premium shallots. Cultivated in fields, our shallots are renowned for their rich flavor and delicate aroma. Grown without harmful chemicals, our shallots offer a unique taste and culinary versatility. Elevate your dishes with the exceptional quality of our shallots.",
       category: "vegetables"
     },
     {
@@ -123,7 +123,7 @@ export class ProductService {
       scientificName: 'Zingiber officinale',
       color: '#E8B786B0',
       imageLink: "../../assets/images/ginger.jpg",
-      description: "Discover the vibrant warmth of our premium ginger. Grown in farms, our ginger is cultivated without synthetic pesticides, preserving its natural flavor and health benefits. Experience the distinctive zing and aromatic essence of our ginger, a versatile spice that adds depth to culinary creations.",
+      description: "Discover the vibrant warmth of our premium ginger. Grown in farms, our ginger is cultivated without harmful chemicals, preserving its natural flavor and health benefits. Experience the distinctive zing and aromatic essence of our ginger, a versatile spice that adds depth to culinary creations.",
       category: "vegetables"
     },
     {
@@ -132,7 +132,7 @@ export class ProductService {
       scientificName: 'Allium sativum',
       color: '#A5A1ACB0',
       imageLink: "../../assets/images/garlic.jpg",
-      description: "Experience the pungent and flavorful essence of our premium garlic.Grown in farms, our garlic is free from synthetic pesticides, ensuring its pure and natural taste.Enjoy the culinary versatility and numerous health benefits of our garlic, a staple ingredient in kitchens worldwide.",
+      description: "Experience the pungent and flavorful essence of our premium garlic.Grown in farms, our garlic is free from harmful chemicals, ensuring its pure and natural taste.Enjoy the culinary versatility and numerous health benefits of our garlic, a staple ingredient in kitchens worldwide.",
       category: "vegetables"
     },
     {
@@ -141,7 +141,7 @@ export class ProductService {
       scientificName: 'Capsicum annuum',
       color: '#6AA801B0',
       imageLink: "../../assets/images/chilli.jpg",
-      description: "Ignite your taste buds with the fiery goodness of our premium green chillies. Grown in farms, our green chillies are free from synthetic pesticides, delivering a vibrant and spicy flavor. Experience the perfect balance of heat and freshness with our green chillies, a must-have ingredient in various cuisines.",
+      description: "Ignite your taste buds with the fiery goodness of our premium green chillies. Grown in farms, our green chillies are free from harmful chemicals, delivering a vibrant and spicy flavor. Experience the perfect balance of heat and freshness with our green chillies, a must-have ingredient in various cuisines.",
       category: "vegetables"
     },
     {
@@ -150,7 +150,7 @@ export class ProductService {
       scientificName: 'Zea mays',
       color: '#E4CEA1B0',
       imageLink: "../../assets/images/maize.jpg",
-      description: "Experience the wholesome goodness of our premium organic dried maize (cholam). Cultivated in organic farms, our maize is carefully dried to retain its nutritional value. Free from synthetic pesticides, our organic maize is versatile and can be used in a variety of culinary preparations. Discover the natural goodness of organic cholam with us.",
+      description: "Experience the wholesome goodness of our premium organic dried maize (cholam). Cultivated in organic farms, our maize is carefully dried to retain its nutritional value. Free from harmful chemicals, our organic maize is versatile and can be used in a variety of culinary preparations. Discover the natural goodness of organic cholam with us.",
       category: 'millets'
     },
     {
@@ -159,7 +159,7 @@ export class ProductService {
       scientificName: 'Pennisetum glaucum',
       color: '#EFE1CAB0',
       imageLink: "../../assets/images/pearlmillet.jpg",
-      description: "Embrace the nutritional power of our premium organic Pearl Millet (Kambu). Sourced from organic farms, our millet is packed with essential nutrients. Cultivated without synthetic pesticides, our organic Pearl Millet offers a nutty flavor and versatility for various culinary creations. Discover the wholesome benefits of organic Kambu with us.",
+      description: "Embrace the nutritional power of our premium organic Pearl Millet (Kambu). Sourced from organic farms, our millet is packed with essential nutrients. Cultivated without harmful chemicals, our organic Pearl Millet offers a nutty flavor and versatility for various culinary creations. Discover the wholesome benefits of organic Kambu with us.",
       category: 'millets'
     },
     {
@@ -168,7 +168,7 @@ export class ProductService {
       scientificName: '',
       color: '#855C3BB0',
       imageLink: "../../assets/images/ragi.jpg",
-      description: "Unlock the nutritional treasure of our premium organic Finger Millet (Ragi/Keppai). Sourced from organic farms, our millet is rich in fiber, calcium, and iron. Grown without synthetic pesticides, our organic Finger Millet offers a unique nutty flavor and is a versatile ingredient for healthy and delicious meals. Discover the goodness of organic Ragi/Keppai with us.",
+      description: "Unlock the nutritional treasure of our premium organic Finger Millet (Ragi/Keppai). Sourced from organic farms, our millet is rich in fiber, calcium, and iron. Grown without harmful chemicals, our organic Finger Millet offers a unique nutty flavor and is a versatile ingredient for healthy and delicious meals. Discover the goodness of organic Ragi/Keppai with us.",
       category: 'millets'
     },
     {
@@ -177,7 +177,7 @@ export class ProductService {
       scientificName: 'Eleusine coracana',
       color: '#CDBA95B0',
       imageLink: "../../assets/images/foxtailmillet.jpg",
-      description: "Discover the ancient grain's charm with our premium organic Foxtail Millet (Thinai). Sourced from organic farms, our millet is rich in fiber, protein, and essential nutrients. Grown without synthetic pesticides, our organic Foxtail Millet offers a nutty flavor and versatility for nutritious and flavorful culinary creations. Experience the goodness of organic Thinai with us.",
+      description: "Discover the ancient grain's charm with our premium organic Foxtail Millet (Thinai). Sourced from organic farms, our millet is rich in fiber, protein, and essential nutrients. Grown without harmful chemicals, our organic Foxtail Millet offers a nutty flavor and versatility for nutritious and flavorful culinary creations. Experience the goodness of organic Thinai with us.",
       category: 'millets'
     },
     {
@@ -186,7 +186,7 @@ export class ProductService {
       scientificName: 'Paspalum scrobiculatum',
       color: '#DDBC6FB0',
       imageLink: "../../assets/images/kodomillet.jpg",
-      description: "Experience the nutritional marvel of our premium organic Kodo Millet (Varagu). Sourced from organic farms, our millet is a rich source of dietary fiber and essential minerals. Grown without synthetic pesticides, our organic Kodo Millet offers a nutty flavor and is a versatile grain for healthy and wholesome meals. Discover the benefits of organic Varagu with us.",
+      description: "Experience the nutritional marvel of our premium organic Kodo Millet (Varagu). Sourced from organic farms, our millet is a rich source of dietary fiber and essential minerals. Grown without harmful chemicals, our organic Kodo Millet offers a nutty flavor and is a versatile grain for healthy and wholesome meals. Discover the benefits of organic Varagu with us.",
       category: 'millets'
     },
     {
@@ -195,7 +195,7 @@ export class ProductService {
       scientificName: 'Panicum sumatrense',
       color: '#D7B186B0',
       imageLink: "../../assets/images/littlemillet.jpg",
-      description: "Discover the nutritional treasure of our premium organic Little Millet (Saamai). Sourced from organic farms, our millet is packed with fiber, protein, and essential minerals. Grown without synthetic pesticides, our organic Little Millet offers a delicate flavor and is perfect for creating healthy and delicious dishes. Experience the goodness of organic Saamai with us.",
+      description: "Discover the nutritional treasure of our premium organic Little Millet (Saamai). Sourced from organic farms, our millet is packed with fiber, protein, and essential minerals. Grown without harmful chemicals, our organic Little Millet offers a delicate flavor and is perfect for creating healthy and delicious dishes. Experience the goodness of organic Saamai with us.",
       category: 'millets'
     },
     {
@@ -204,7 +204,7 @@ export class ProductService {
       scientificName: 'Echinochloa esculenta',
       color: '#BF9E74B0',
       imageLink: "../../assets/images/barnyardmillet.jpg",
-      description: "Experience the wholesome goodness of our premium organic Barnyard Millet (Kuthiraivali). Sourced from organic farms, our millet is rich in fiber, iron, and essential nutrients. Grown without synthetic pesticides, our organic Barnyard Millet offers a nutty flavor and is a versatile grain for nutritious and tasty culinary creations. Discover the benefits of organic Kuthiraivali with us.",
+      description: "Experience the wholesome goodness of our premium organic Barnyard Millet (Kuthiraivali). Sourced from organic farms, our millet is rich in fiber, iron, and essential nutrients. Grown without harmful chemicals, our organic Barnyard Millet offers a nutty flavor and is a versatile grain for nutritious and tasty culinary creations. Discover the benefits of organic Kuthiraivali with us.",
       category: 'millets'
     },
     {
@@ -213,7 +213,7 @@ export class ProductService {
       scientificName: 'Sorghum bicolor',
       color: '#E1C89FB0',
       imageLink: "../../assets/images/sorghum.jpg",
-      description: "Explore the nutritional prowess of our premium organic Sorghum (Jowar). Sourced from organic fields, our sorghum is rich in fiber, protein, and essential minerals. Cultivated without synthetic pesticides, our organic Jowar offers a mild, nutty flavor and is a versatile grain for creating wholesome and hearty meals. Experience the goodness of organic Sorghum with us.",
+      description: "Explore the nutritional prowess of our premium organic Sorghum (Jowar). Sourced from organic fields, our sorghum is rich in fiber, protein, and essential minerals. Cultivated without harmful chemicals, our organic Jowar offers a mild, nutty flavor and is a versatile grain for creating wholesome and hearty meals. Experience the goodness of organic Sorghum with us.",
       category: 'millets'
     },
     {
